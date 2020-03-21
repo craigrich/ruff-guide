fd7a069eb0742ae5f1b4e192e047fa85dbe7bc33
Craig Richardson <craig.rich@hotmail.co.uk>
2020-03-21 09:12:07 +0000

    Fix Static Routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+const fetch = require('isomorphic-unfetch');
+
 module.exports = {
+  exportTrailingSlash: true,
+  exportPathMap: async function() {
+    const paths = {
+      '/': { page: '/' },
+      '/region/devon': { page: '/Region/[id]' }
+    };
+    // Use this for dymanic routes
+    // const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    // const data = await res.json();
+    // const shows = data.map(entry => entry.show);
+    // shows.forEach(show => {
+    //   paths[`/region/${show.id}`] = {
+    //     page: '/Region/[id]'
+    //   };
+    // });
+    return paths;
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.modules = [__dirname, "node_modules"];
+    config.resolve.modules = [__dirname, 'node_modules'];
     return config;
   }
 };
